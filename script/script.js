3417140126356addb069e8ab842cad1597710e59
iamlimo <isaac.orija@gmail.com>
2019-02-14 11:18:50 +0100

    updated index html

--- script/script.js
+++ script/script.js
@@ -1,6 +1,29 @@
-var todo = () => {
- let tasks = [];
+var inputVal = $('#formField').val()
+var todoList = $('#todolist');
 
+
+var todos = [];
+function createTodo(item){
+// Return a todo object
 }
 
-todo()
+function removeTodo(index){
+    // Remove todo from todos at index
+}
+
+function addTodo(todo){
+//Add todo to todos
+
+todos.unshift($('#formField').val())
+}
+
+function renderTodos(){
+    $('.todolist').prepend(`<li> ${$('#formField').val()}<span class delete> X </span> </li>`);
+}
+
+$('#subBut').click(function (e) { 
+    
+    addTodo(inputVal);
+    renderTodos()
+    
+});
