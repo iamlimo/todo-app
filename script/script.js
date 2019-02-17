9aa31b63cbecf5ee85231185c8b5a7932aca635e
iamlimo <isaac.orija@gmail.com>
2019-02-17 12:37:06 +0100

    index.html

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 var inputVal = $('#formField').val()
-var todoList = $('#todolist');
+var todoList = $('.todolist');
 
 
 var todos = [];
@@ -7,10 +7,31 @@ function createTodo(item){
 // Return a todo object
 }
 
-function removeTodo(index){
+function removeTodo(){
     // Remove todo from todos at index
+    var tos =  $(".tos")
+    $('.delete').click(function (e) { 
+    e.preventDefault();
+   
+ 
+    if (e.target.tagName === 'SPAN'){
+        $(e.target).closest('li').hide(400);
+       };
+    
+});
+    
 }
 
+function done(){
+    $('.todolist').click(function (e) { 
+      e.preventDefault();
+      if (e.target.tagName === 'LI'){
+        $(e.target).toggleClass('checked');
+       };
+      
+  });
+}
+done()
 function addTodo(todo){
 //Add todo to todos
 
@@ -18,12 +39,13 @@ todos.unshift($('#formField').val())
 }
 
 function renderTodos(){
-    $('.todolist').prepend(`<li> ${$('#formField').val()}<span class delete> X </span> </li>`);
+    $('.todolist').prepend(`<li class="tos"> ${$('#formField').val()}<span class="delete"> X </span> </li>`);
+    $('#formField').val("")
 }
 
 $('#subBut').click(function (e) { 
-    
+    // done();
     addTodo(inputVal);
     renderTodos()
-    
+    removeTodo()
 });
